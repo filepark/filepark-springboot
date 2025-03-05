54325f9632b2cab83223798c7582d97e98fdf8cb
jw6963 <jw6963@naver.com>
2025-03-06 00:19:42 +0900

    kakao login

--- src/main/java/service/UsersService.java
+++ src/main/java/service/UsersService.java
@@ -1,5 +1,7 @@
 package service;
 
+import mapper.UsersMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
@@ -7,5 +9,12 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class UsersService {
-
+    @Autowired
+    UsersMapper usersMapper;
+    public void signUp(String name, String profileImage) {
+        usersMapper.signUp(name, profileImage);
+    }
+    public int chkMember(String name, String profileImage) {
+        return usersMapper.chkMember(name, profileImage);
+    }
 }
